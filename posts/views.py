39fabd73702f121f3df3d59de224693b21f51920
ChanForPres <marcuschan999@gmail.com>
2019-03-05 17:22:54 -0800

    finished basic ver

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+# posts/views.py
+from django.views.generic import ListView, CreateView # new
+from django.urls import reverse_lazy # new
 
-# Create your views here.
+from .forms import PostForm # new
+from .models import Post
+
+class HomePageView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+class CreatePostView(CreateView): # new
+    model = Post
+    form_class = PostForm
+    template_name = 'post.html'
+    success_url = reverse_lazy('home')
